5f8997c657532cc37bcced1908da59517f3404fc
“thaotran“ <phtranthao@gmail.com>
2024-07-06 16:12:22 +1000

    Add new project details

---
+++ color-clash.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    $("#navbar").load("navbar.html", function () {
+      const themeToggleBtn = document.querySelector("[data-theme-toggle]");
+      if (themeToggleBtn) {
+        themeToggleBtn.addEventListener('click', toggleTheme);
+      } else {
+        console.error('Theme toggle button not found');
+      }
+    });
+  
+      //Load footer
+    $("#footer").load("footer.html");
+  });
+  
